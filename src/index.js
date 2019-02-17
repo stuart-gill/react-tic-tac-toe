a473ab68f79c44e4b3751dedfb451ee083032f3a
Stuart Gill <stuart.a.gill@gmail.com>
2019-02-17 09:48:52 -0800

    game functioning with xs and os, no game reset yet

--- src/index.js
+++ src/index.js
@@ -30,12 +30,21 @@ class Board extends React.Component {
 
   handleClick(i) {
     const newSquares = this.state.squares.slice();
-    newSquares[i] = this.state.xIsNext ? "X" : "Dick";
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = this.state.xIsNext ? "X" : "0";
     this.setState({ squares: newSquares, xIsNext: !this.state.xIsNext });
   }
 
   render() {
-    const status = "Next player: " + (this.state.xIsNext ? "X" : "Dick");
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner ${winner}`;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "0");
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -73,6 +82,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
